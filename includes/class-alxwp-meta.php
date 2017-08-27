2dd0a2f5282ab3e26b77d372efb6865c19b83dd8
Alexandru Doda <doda@alexandru.co>
2017-08-27 20:55:31 +0300

    Extra Fields & Structure

--- includes/class-alxwp-meta.php
+++ includes/class-alxwp-meta.php
@@ -57,6 +57,137 @@ if( ! class_exists( 'ALXWP_Meta' ) ) {
 		}
 
 		/**
+		 * Register the plugin meta boxes.
+		 *
+		 * @link https://codex.wordpress.org/Plugin_API/Action_Reference/add_meta_boxes
+		 */
+		public function add_meta_boxes() {
+		
+			foreach( $this->meta_boxes as $meta_box ) {
+				add_meta_box( 
+					$meta_box['id'] . '_meta_box', 
+					$meta_box['title'], 
+					array( $this, 'meta_box' ), 
+					$this->post_type, 
+					$meta_box['context'], 
+					$meta_box['priority'] 
+				);
+			}
+
+		}
+
+		/**
+		 * Generate the markup for a meta box.
+		 *
+		 * @param     int    $post    The post id to register the meta box for.
+		 * @return    void
+		 */
+		public function meta_box( $post ) {
+
+			// Retrieve the current values.
+			$meta_box = $this->meta_boxes[ $this->counter ];
+			
+			// Make sure the form request comes from WordPress.
+			wp_nonce_field( basename( __FILE__ ), $meta_box['id'] . '_meta_box_nonce' );
+
+			// Get the current data from the database.
+			foreach( $meta_box['fields'] as $field ) { 
+				
+				// Retrieve the current field value.
+				$value = get_post_meta( $post->ID, '_' . $field['id'], true );
+
+				// If none is set, fallback to default.
+				if( empty( $value ) && isset( $field['default'] ) && !in_array( $field['type'], array( 'checkbox', 'toggle' ) ) ) {
+					update_post_meta( $post->ID, '_' . $field['id'], $field['default'] );
+					$value = get_post_meta( $post->ID, '_' . $field['id'], true );
+				}
+				
+				// Format the field for checkboxes and toggles.
+				if( $field['type'] == 'checkbox' || $field['type'] == 'toggle' ) {
+					$value = ( $value ) ? $value : array();
+				} ?>
+
+				<div id="<?php echo $meta_box['id'] ?>_<?php echo $field['id'] ?>" class="field">
+
+					<p class="label">
+						<label for="<?php echo $field['id'] ?>">
+							<strong><?php echo $field['title'] ?></strong>
+						</label>
+					</p>
+
+					<span class="value">
+						<?php echo $this->field( $field, $value ); ?>
+					</span>
+
+				</div>
+
+			<?php }
+
+			// Increment the class meta counter.
+			$this->counter += 1;
+
+		}
+
+		/**
+		 * Store custom field meta box data
+		 *
+		 * @link     https://codex.wordpress.org/Plugin_API/Action_Reference/save_post
+		 * @param    int    $post_id    The post ID.
+		*/
+		public function save_meta_boxes( $post_id ) {
+			
+			// Do not run if it's an autosave.
+			if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ){
+				return;
+			}
+			
+			// Check if the user is allowed to edit the post.
+			if ( ! current_user_can( 'edit_post', $post_id ) ){
+				return;
+			}
+
+			// Process each meta box.
+			foreach( $this->meta_boxes as $meta_box ) {
+
+				// Verify the meta box nonce.
+				if ( !isset( $_POST[ $meta_box['id'] . '_meta_box_nonce' ] ) || !wp_verify_nonce( $_POST[ $meta_box['id'] . '_meta_box_nonce' ], basename( __FILE__ ) ) ){
+					return;
+				}
+
+				// Save the data based on field type.
+				foreach( $meta_box['fields'] as $field ) {
+
+					if( $field['type'] == 'checkbox' || $field['type'] == 'toggle' ) {
+
+						/**
+						 * Checkbox & Toggle
+						 */
+						
+						if( isset( $_POST[ $field['id'] ] ) ){
+							update_post_meta( $post_id, '_' . $field['id'], array_map( 'sanitize_text_field', (array) $_POST[ $field['id'] ] ) );
+						} else {
+							delete_post_meta( $post_id, '_' . $field['id'] );
+						}
+
+					} else {
+
+						/**
+						 * Default
+						 */
+						
+						if ( isset( $_REQUEST[ $field['id'] ] ) ) {
+							update_post_meta( $post_id, '_' . $field['id'], sanitize_text_field( $_POST[ $field['id'] ] ) );
+						}
+
+					}
+
+				}
+
+			}
+
+		}
+
+		/**
 		 * Field
 		 *
 		 * @param     object    $field
@@ -64,32 +195,8 @@ if( ! class_exists( 'ALXWP_Meta' ) ) {
 		 * @return    string    The field.
 		 */
 		public function field( $field, $value ) {
-
+			
 			switch( $field['type'] ) {
-
-				case 'text':
-					return $this->text( $field, $value );
-				break;
-
-				case 'number':
-					return $this->text( $field, $value );
-				break;
-
-				case 'phone':
-					return $this->text( $field, $value );
-				break;
-
-				case 'url':
-					return $this->text( $field, $value );
-				break;
-
-				case 'email':
-					return $this->text( $field, $value );
-				break;
-
-				case 'password':
-					return $this->text( $field, $value );
-				break;
 
 				case 'textarea':
 					return $this->textarea( $field, $value );
@@ -103,6 +210,18 @@ if( ! class_exists( 'ALXWP_Meta' ) ) {
 					return $this->select( $field, $value );
 				break;
 
+				case 'toggle':
+					return $this->toggle( $field, $value );
+				break;
+
+				case 'checkbox':
+					return $this->checkbox( $field, $value );
+				break;
+
+				case 'radio':
+					return $this->radio( $field, $value );
+				break;
+
 				default:
 					return $this->text( $field, $value );
 				break;
@@ -113,6 +232,8 @@ if( ! class_exists( 'ALXWP_Meta' ) ) {
 
 		/**
 		 * Field: Single Line Text
+		 * 
+		 * This field is used for text, phone, email and url inputs.
 		 *
 		 * @param     object    $field
 		 * @param     string    $value
@@ -126,7 +247,7 @@ if( ! class_exists( 'ALXWP_Meta' ) ) {
 			}
 
 			// Return the input.
-			return '<input type="' . $field['type'] . '" name="' . $field['id'] . '" id="' . $field['id'] . '" class="widefat" value="' . esc_attr( $value ) . '">';
+			return '<input type="' . $field['type'] . '" name="' . $field['id'] . '" id="' . $field['id'] . '" class="widefat" value="' . $value . '">';
 
 		}
 
@@ -165,7 +286,7 @@ if( ! class_exists( 'ALXWP_Meta' ) ) {
 		 */
 		public function select( $field, $value ) {
 			
-			$markup = '<select name="' . $field['id'] . '">';
+			$markup = '<select name="' . $field['id'] . '" id="' . $field['id'] . '">';
 			foreach( $field['options'] as $option ) {
 				$markup .= '<option value="' . $option['id'] . '"' . selected( $value, $option['id'], false ) . '>' . $option['title'] . '</option>';
 			}
@@ -173,100 +294,63 @@ if( ! class_exists( 'ALXWP_Meta' ) ) {
 			return $markup;
 
 		}
-		
+
 		/**
-		 * Register the plugin meta boxes.
+		 * Field: Toggle
 		 *
-		 * @link https://codex.wordpress.org/Plugin_API/Action_Reference/add_meta_boxes
+		 * @param     object    $field
+		 * @param     string    $value
+		 * @return    string    The toggle field.
 		 */
-		public function add_meta_boxes() {
-		
-			foreach( $this->meta_boxes as $meta_box ) {
-				add_meta_box( 
-					$meta_box['id'] . '_meta_box', 
-					$meta_box['title'], 
-					array( $this, 'meta_box' ), 
-					$this->post_type, 
-					$meta_box['context'], 
-					$meta_box['priority'] 
-				);
-			}
+		public function toggle( $field, $value ) {
+
+			$markup  = '<input type="checkbox" name="' . $field['id'] . '[]" id="' . $field['id'] . '" value="' . $field['id'] . '"' . checked( ( in_array( $field['id'], $value ) ) ? $field['id'] : '', $field['id'], false ) . '>';
+			$markup .= '<label for="' . $field['id'] . '">' . $field['description'] . '</label>';
+
+			return $markup;
 
 		}
 
 		/**
-		 * Generate the markup for a meta box.
+		 * Field: Checkbox
 		 *
-		 * @param     int    $post    The post id to register the meta box for.
-		 * @return    void
+		 * @param     object    $field
+		 * @param     string    $value
+		 * @return    string    The checkbox field.
 		 */
-		public function meta_box( $post ) {
-			
-			// Retrieve the current values.
-			$meta_box = $this->meta_boxes[ $this->counter ];
+		public function checkbox( $field, $value ) {
 
-			// Make sure the form request comes from WordPress
-			wp_nonce_field( basename( __FILE__ ), $meta_box['id'] . '_meta_box_nonce' );
+			$markup = '';
 
-			// Get the current data from the database.
-			foreach( $meta_box['fields'] as $field ) { 
+			foreach ( $field['options'] as $option ) {
 
-				// Attempt to get the current field value.
-				$value = get_post_meta( $post->ID, '_' . $meta_box['id'] . '_' . $field['id'], true ); 
-				
-				// If none is set, fallback to default.
-				if( empty( $value ) && isset( $field['default'] ) ) {
-					update_post_meta( $post->ID, '_' . $meta_box['id'] . '_' . $field['id'], $field['default'] );
-					$value = get_post_meta( $post->ID, '_' . $meta_box['id'] . '_' . $field['id'], true );
-				} ?>
+				$markup .= '<input type="checkbox" name="' . $field['id'] . '[]" id="' . $field['id'] . '[' . $option['id'] . ']' . '" value="' . $option['id'] . '"' . checked( ( in_array( $option['id'], $value ) ) ? $option['id'] : '', $option['id'], false ) . '>';
+				$markup .= $option['title'];
+				$markup .= '<br />';
 
-				<div id="<?php echo $meta_box['id'] ?>_<?php echo $field['id'] ?>" class="field">
-					<p class="label">
-						<label for="<?php echo $field['id'] ?>">
-							<strong><?php echo $field['title'] ?></strong>
-						</label>
-					</p>	
-					<span class="value">
-						<?php echo $this->field( $field, $value ); ?>
-					</span>
-				</div>
+			}
 
-			<?php }
-
-			// Increment the class meta counter.
-			$this->counter += 1;
+			return $markup;
 
 		}
 
 		/**
-		 * Store custom field meta box data
+		 * Field: Radio
 		 *
-		 * @link     https://codex.wordpress.org/Plugin_API/Action_Reference/save_post
-		 * @param    int    $post_id    The post ID.
-		*/
-		public function save_meta_boxes( $post_id ) {
-			
-			// Do not run if it's an autosave.
-			if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ){
-				return;
+		 * @param     object    $field
+		 * @param     string    $value
+		 * @return    string    The radio field.
+		 */
+		public function radio( $field, $value ) {
+
+			$markup = '';
+
+			foreach( $field['options'] as $option ) {
+				$markup .= '<input type="radio" name="' . $field['id'] . '" id="' . $field['id'] . '[' . $option['id'] . ']' . '" value="' . $option['id'] . '"' . checked( $value, $option['id'], false ) . '>';
+				$markup .= $option['title'] . '<br />';
 			}
 
-			// Check if the user is allowed to edit the post.
-			if ( ! current_user_can( 'edit_post', $post_id ) ){
-				return;
-			}
-
-			// Check if there's a valid nonce and update the values.
-			foreach( $this->meta_boxes as $meta_box ) {
-				if ( !isset( $_POST[ $meta_box['id'] . '_meta_box_nonce' ] ) || !wp_verify_nonce( $_POST[ $meta_box['id'] . '_meta_box_nonce' ], basename( __FILE__ ) ) ){
-					return;
-				}
-				foreach( $meta_box['fields'] as $field ) {
-					if ( isset( $_REQUEST[ $field['id'] ] ) ) {	
-						update_post_meta( $post_id, '_' . $meta_box['id'] . '_' . $field['id'], sanitize_text_field( $_POST[ $field['id'] ] ) );
-					}
-				}
-			}
+			return $markup;
 
 		}
 
